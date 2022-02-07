04324b74fa529a223212e5230b2f227503c7385a
Jimmy <dm.a.uvarov@gmail.com>
2022-02-07 17:13:55 -0500

    commit tree 5c46b7b969a3bdeaa9c8b4416e1b006d26e3a699

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,32 @@
-import React from "react";
+import React,{useState} from "react";
+
+
 
 function Item({ name, category }) {
+  
+  // state tulpe
+  const [isIncart, PutInCart] = useState(false);
+
+  // className changer based on state value
+  const liNameClass = isIncart ? "in-cart" : "";
+
+  // text btn changer based on state value
+  const BtnName = isIncart ? "Remove From Cart" : "Add To Cart";
+
+  // className of btn changer based on state incart value
+  const BtnClass = !isIncart ? "add":"remove";
+
+// handler function on click add to cart btn 
+  function handleOnClick(){
+      PutInCart(!isIncart);
+  }
+  
+  
   return (
-    <li className="">
+    <li className={liNameClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick= {handleOnClick}className={BtnClass}>{BtnName}</button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
+
+
+
 function ShoppingList({ items }) {
+
+  // state tulpe
+  const [filteredBy,setFilteredBy] = useState("All");
+
+  // const confirmFilter = e => setFilteredBy(e.target.value);
+
+  
+
+  // const filteredItems = () => {
+  //   if (option == ""){
+  //     return items
+  //   } else{
+  //     return items.filter(item => {
+  //       item.category === confirmFilter
+  //     })
+  //   }
+  // }
+
+  // const ItemsToDisplay = items.filter(item =>{
+  //     if (filteredBy === "All"){
+  //       return true;
+  //     } else{
+  //       return item.category === filteredBy
+  //     }
+  //     );
+
+ let filteredItems ;
+
+  function changeFilterHandler(option) {
+  
+    setFilteredBy(option);
+    // filteredItems = (filteredBy==="All") ? [...items] : (items.filter(item => 
+    //   item.category === filteredBy));
+
+
+    // console.log("chosen option:",option);
+    // console.log("changed state:",filteredBy);
+    // console.log(filteredItems);
+  }
+
+function onlyFilteredItems(arItems){
+
+  filteredItems = arItems.filter(item => (filteredBy ==="All" ? item : item.category === filteredBy))
+}
+onlyFilteredItems(items);
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={e=>changeFilterHandler(e.target.value)} >
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,7 +62,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {filteredItems.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
